356a849f258f69d734bf17b6a3a0d21b25166f9c
Vital <vgf@umich.edu>
2024-05-09 01:40:54 -0300

    Specsy 0.2.8

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,7 +32,7 @@ copyright = '2022, Vital-Fernandez'
 author = 'Vital-Fernandez'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.7'
+release = '0.2.8'
 
 
 # -- General configuration ---------------------------------------------------
